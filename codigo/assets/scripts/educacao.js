1528c70582c85dd5a938d05184d7a552f5850243
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-06-23 22:01:21 -0300

    att

--- codigo/assets/scripts/educacao.js
+++ codigo/assets/scripts/educacao.js
@@ -1,157 +1,325 @@
 var biblioteca = {
-    "livros": [
-      {
-        "id": "1",
-        "tema": "Finanças",
-        "tipo": "Artigo | SEBRAE | Português",
-        "imagem": "gestaopequenonegocio.jpg",
-        "nome": "Como fazer a gestão financeira do pequeno negócio. | Investimentos e Crises.",
-        "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/como-fazer-a-gestao-financeira-do-pequeno-negocio,d999a442d2e5a410VgnVCM1000003b74010aRCRD"
-  
-      },
-      {
-        "id": "2",
-        "tema": "Educação Financeira",
-        "tipo": "Podcast | Spotify | G1 | Português",
-        "imagem": "financiamentocreditocaro.jpg",
-        "nome": "Crédito está caro: como ficam financiamentos e empréstimos.",
-        "link": "https://open.spotify.com/episode/5f4KrY25VVmGP68RcsMMfN?si=JhpQsqV_QKih6ucqkoEHQg"
-      },
-      {
-        "id": "3",
-        "tema": "Vendas",
-        "tipo": "Artigo | SEBRAE | Português",
-        "imagem": "marketingdigital.jpg",
-        "nome": "Importância do marketing digital nas redes sociais da pequena empresa.",
-        "link": "https://sebrae.com.br/sites/PortalSebrae/importancia-do-marketing-digital-nas-redes-sociais-da-pequena-empresa,21cb6ae54f417810VgnVCM1000001b00320aRCRD"
-      },
-      {
-        "id": "4",
-        "tema": "Finanças",
-        "tipo": "E-Book | SEBRAE | Português",
-        "imagem": "curvaabcefluxodecaixa.jpg",
-        "nome": "Fluxo de caixa e Curva ABC do produto. | Aprenda e melhore sua gestão.",
-        "link": "https://sebrae.com.br/sites/PortalSebrae/arquivos/fluxo-de-caixa-e-curva-abc-do-produto,a9663256cb597810VgnVCM1000001b00320aRCRD"
-  
-      },
-      {
-        "id": "5",
-        "tema": "Finanças",
-        "tipo": "Vídeos | SEBRAE | Português",
-        "imagem": "emitirnotasfiscais.jpg",
-        "nome": "Como emitir Notas Fiscais. | Uma série de vídeos sobre o tema.",
-        "link": "https://sebrae.com.br/sites/PortalSebrae/produtoseservicos/emissornfe/os-servicos-disponiveis-no-novo-emissor,f1883256cb597810VgnVCM1000001b00320aRCRD"
-  
-      },
-      {
-        "id": "6",
-        "tema": "Finanças",
-        "tipo": "Artigo | SEBRAE | Português",
-        "imagem": "conhecendolinhasdecredito.jpg",
-        "nome": "Conheça as linhas de crédito disponíveis para MEI.",
-        "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/conheca-as-linhas-de-credito-disponiveis-para-mei,cb7b198074952810VgnVCM100000d701210aRCRD"
-  
-      },
-      {
-        "id": "7",
-        "tema": "Educação Financeira",
-        "tipo": "Podcast | Spotify | G1 | Português",
-        "imagem": "investierrado.jpg",
-        "nome": "Investi errado, e agora? | Participação da Sharon Halpern.",
-        "link": "https://open.spotify.com/episode/22QFVvdBdeFOWd0EZ18B2x?si=0YqOBv6zQSOpvDh4nbf6Qw"
-  
-      },
-      {
-        "id": "8",
-        "tema": "Finanças",
-        "tipo": "Artigo | SEBRAE | Português",
-        "imagem": "garantiadecredito.jpg",
-        "nome": "Conheça uma opção de garantia de crédito para pequenos negócios.",
-        "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/conheca-opcoes-de-garantia-de-credito-para-pequenos-negocios,a6dea442d2e5a410VgnVCM1000003b74010aRCRD"
-  
-      },
-      {
-        "id": "9",
-        "tema": "Educação Financeira",
-        "tipo": "Podcast | Spotify | G1 | Português",
-        "imagem": "vendasmarketplace.jpg",
-        "nome": "Lições para entrar - e arrasar nas vendas - em um marketplace.",
-        "link": "https://open.spotify.com/episode/58GOtrjuczXrTz2jh4OfpQ?si=mkToX9YXSCa_fll3mnvVtw"
-  
-      },
-      {
-        "id": "10",
-        "tema": "Planejamento",
-        "tipo": "Artigo | SEBRAE | Português",
-        "imagem": "iniciarumnegocio.jpg",
-        "nome": "Como se preparar para iniciar um negócio: Reflexão, planejamento e custos.",
-        "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/como-se-preparar-para-iniciar-um-negocio,a63555c941726810VgnVCM1000001b00320aRCRD"
-  
-      },
-      {
-        "id": "11",
-        "tema": "Planejamento",
-        "tipo": "Artigo | SEBRAE | Português",
-        "imagem": "expandirempresa.jpg",
-        "nome": "Quer expandir a sua empresa? Saiba como fazer.",
-        "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/quer-expandir-a-sua-empresa-saiba-como-fazer,cd35eef40ce06810VgnVCM1000001b00320aRCRD"
-  
-      },
-      {
-        "id": "12",
-        "tema": "Planejamento",
-        "tipo": "Artigo | SEBRAE | Português",
-        "imagem": "planejamentofinanceiro.jpg",
-        "nome": "O que é um planejamento empresarial? | Esteja preparado para qualquer adversidade.",
-        "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/o-que-e-um-planejamento-empresarial,8ba3b543515d2810VgnVCM100000d701210aRCRD"
-  
-      }
-    ]
-  }
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const filtroTema = document.getElementById("filtro-tema");
-    const filtroTipo = document.getElementById("filtro-tipo");
-    const cardsContainer = document.getElementById("cards-container");
-  
-    function exibirCards() {
-      cardsContainer.innerHTML = "";
-  
-      const temaFiltrado = filtroTema.value;
-      const tipoFiltrado = filtroTipo.value;
-  
-      const cardsFiltrados = biblioteca.livros.filter(function (livro) {
-        if ((temaFiltrado && temaFiltrado !== livro.tema) || (tipoFiltrado && tipoFiltrado !== livro.tipo)) {
-          return false;
-        }
-  
-        return true;
-      });
-  
-      cardsFiltrados.forEach(function (livro) {
-        const card = document.createElement("col");
-        card.classList.add("col-lg-3", "col-md-6", "col-sm-12", "card");
-  
-        card.innerHTML = `
-          <div class="card-body">
-            <h5 class="card-title">${livro.tema}</h5>
-            <p class="card-text">${livro.tipo}</p>
-            <div class="ratio ratio-1x1">
-              <img src="./assets/images/${livro.imagem}" class="card-img-top" style="object-fit: contain;">
-            </div>
-            <div class="content">
-              <p class="card-nome">${livro.nome}</p>
-              <a href="${livro.link}" class="btn btn-outline-primary w-100">Leia aqui</a>
-            </div>
-          </div>
-        `;
-  
-        cardsContainer.appendChild(card);
-      });
+  "livros": [
+    {
+      "id": "1",
+      "tema": "Finanças",
+      "tipo": "Artigo",
+      "imagem": "./assets/images/gestaopequenonegocio.jpg",
+      "nome": "Como fazer a gestão financeira do pequeno negócio. | Investimentos e Crises.",
+      "detalhes": "SEBRAE | Português",
+      "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/como-fazer-a-gestao-financeira-do-pequeno-negocio,d999a442d2e5a410VgnVCM1000003b74010aRCRD"
+
+    },
+    {
+      "id": "2",
+      "tema": "Educação Financeira",
+      "tipo": "Podcast",
+      "imagem": "./assets/images/financiamentocreditocaro.jpg",
+      "nome": "Crédito está caro: como ficam financiamentos e empréstimos.",
+      "detalhes": "Spotify | G1 | Português",
+      "link": "https://open.spotify.com/episode/5f4KrY25VVmGP68RcsMMfN?si=JhpQsqV_QKih6ucqkoEHQg"
+    },
+    {
+      "id": "3",
+      "tema": "Vendas",
+      "tipo": "Artigo",
+      "imagem": "./assets/images/marketingdigital.jpg",
+      "nome": "Importância do marketing digital nas redes sociais da pequena empresa.",
+      "detalhes": "SEBRAE | Português",
+      "link": "https://sebrae.com.br/sites/PortalSebrae/importancia-do-marketing-digital-nas-redes-sociais-da-pequena-empresa,21cb6ae54f417810VgnVCM1000001b00320aRCRD"
+    },
+    {
+      "id": "4",
+      "tema": "Finanças",
+      "tipo": "E-Book",
+      "imagem": "./assets/images/curvaabcefluxodecaixa.jpg",
+      "nome": "Fluxo de caixa e Curva ABC do produto. | Aprenda e melhore sua gestão.",
+      "detalhes": "SEBRAE | Português",
+      "link": "https://sebrae.com.br/sites/PortalSebrae/arquivos/fluxo-de-caixa-e-curva-abc-do-produto,a9663256cb597810VgnVCM1000001b00320aRCRD"
+
+    },
+    {
+      "id": "5",
+      "tema": "Finanças",
+      "tipo": "Vídeos",
+      "imagem": "./assets/images/emitirnotasfiscais.jpg",
+      "nome": "Como emitir Notas Fiscais. | Uma série de vídeos sobre o tema.",
+      "detalhes": "SEBRAE | Português",
+      "link": "https://sebrae.com.br/sites/PortalSebrae/produtoseservicos/emissornfe/os-servicos-disponiveis-no-novo-emissor,f1883256cb597810VgnVCM1000001b00320aRCRD"
+
+    },
+    {
+      "id": "6",
+      "tema": "Finanças",
+      "tipo": "Artigo",
+      "imagem": "./assets/images/conhecendolinhasdecredito.jpg",
+      "nome": "Conheça as linhas de crédito disponíveis para MEI.",
+      "detalhes": "SEBRAE | Português",
+      "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/conheca-as-linhas-de-credito-disponiveis-para-mei,cb7b198074952810VgnVCM100000d701210aRCRD"
+
+    },
+    {
+      "id": "7",
+      "tema": "Educação Financeira",
+      "tipo": "Podcast",
+      "imagem": "./assets/images/investierrado.jpg",
+      "nome": "Investi errado, e agora? | Participação da Sharon Halpern.",
+      "detalhes": "Spotify | G1 | Português",
+      "link": "https://open.spotify.com/episode/22QFVvdBdeFOWd0EZ18B2x?si=0YqOBv6zQSOpvDh4nbf6Qw"
+
+    },
+    {
+      "id": "8",
+      "tema": "Finanças",
+      "tipo": "Artigo",
+      "imagem": "./assets/images/garantiadecredito.jpg",
+      "nome": "Conheça uma opção de garantia de crédito para pequenos negócios.",
+      "detalhes": "SEBRAE | Português",
+      "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/conheca-opcoes-de-garantia-de-credito-para-pequenos-negocios,a6dea442d2e5a410VgnVCM1000003b74010aRCRD"
+
+    },
+    {
+      "id": "9",
+      "tema": "Educação Financeira",
+      "tipo": "Podcast",
+      "imagem": "./assets/images/vendasmarketplace.jpg",
+      "nome": "Lições para entrar - e arrasar nas vendas - em um marketplace.",
+      "detalhes": "Spotify | G1 | Português",
+      "link": "https://open.spotify.com/episode/58GOtrjuczXrTz2jh4OfpQ?si=mkToX9YXSCa_fll3mnvVtw"
+
+    },
+    {
+      "id": "10",
+      "tema": "Planejamento",
+      "tipo": "Artigo",
+      "imagem": "./assets/images/iniciarumnegocio.jpg",
+      "nome": "Como se preparar para iniciar um negócio: Reflexão, planejamento e custos.",
+      "detalhes": "SEBRAE | Português",
+      "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/como-se-preparar-para-iniciar-um-negocio,a63555c941726810VgnVCM1000001b00320aRCRD"
+
+    },
+    {
+      "id": "11",
+      "tema": "Planejamento",
+      "tipo": "Artigo",
+      "imagem": "./assets/images/expandirempresa.jpg",
+      "nome": "Quer expandir a sua empresa? Saiba como fazer. | Aprenda a estruturar o seu sonho para crescer.",
+      "detalhes": "SEBRAE | Português",
+      "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/quer-expandir-a-sua-empresa-saiba-como-fazer,cd35eef40ce06810VgnVCM1000001b00320aRCRD"
+
+    },
+    {
+      "id": "12",
+      "tema": "Planejamento",
+      "tipo": "Artigo",
+      "imagem": "./assets/images/planejamentofinanceiro.jpg",
+      "nome": "O que é um planejamento empresarial? | Esteja preparado para qualquer adversidade.",
+      "detalhes": "SEBRAE | Português",
+      "link": "https://sebrae.com.br/sites/PortalSebrae/artigos/o-que-e-um-planejamento-empresarial,8ba3b543515d2810VgnVCM100000d701210aRCRD"
+
     }
-  
-    filtroTema.addEventListener("change", exibirCards);
-    filtroTipo.addEventListener("change", exibirCards);
-  
-  });
-  
+  ]
+}
+
+// Função para criar os cards e abrir o modal
+function criarCards() {
+var container = document.getElementById("cards-container");
+
+for (var i = 0; i < biblioteca.livros.length; i++) {
+  var livro = biblioteca.livros[i];
+
+  var card = document.createElement("div");
+  card.className = "col-lg-4 col-md-6 col-sm-12";
+
+  var cardContent = document.createElement("div");
+  cardContent.className = "card h-100";
+
+  var cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+
+  var cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.textContent = livro.tema;
+
+  var cardText = document.createElement("p");
+  cardText.className = "card-text";
+  cardText.textContent = livro.tipo;
+
+  var imageRatio = document.createElement("div");
+  imageRatio.className = "ratio ratio-1x1";
+
+  var cardImage = document.createElement("img");
+  cardImage.className = "card-img-top";
+  cardImage.src = livro.imagem;
+  cardImage.style.objectFit = "contain";
+
+  var contentDiv = document.createElement("div");
+  contentDiv.className = "content";
+
+  var cardNome = document.createElement("p");
+  cardNome.className = "card-nome";
+  cardNome.textContent = livro.nome;
+
+  var button = document.createElement("button");
+  button.className = "btn btn-info w-100";
+  button.textContent = "Veja aqui";
+  button.addEventListener("click", (function (livro) {
+    return function () {
+      abrirModal(livro.detalhes, livro.link);
+    };
+  })(livro));
+
+  contentDiv.appendChild(cardNome);
+  contentDiv.appendChild(button);
+
+  imageRatio.appendChild(cardImage);
+
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  cardBody.appendChild(imageRatio);
+  cardBody.appendChild(contentDiv);
+
+  cardContent.appendChild(cardBody);
+
+  card.appendChild(cardContent);
+
+  container.appendChild(card);
+}
+}
+
+// Função para abrir o modal com os detalhes e o link
+function abrirModal(detalhes, link) {
+var modalDetalhes = document.getElementById("modal-detalhes");
+var modalLink = document.getElementById("modal-link");
+
+modalDetalhes.textContent = detalhes;
+modalLink.href = link;
+
+// Abre o modal
+var modal = new bootstrap.Modal(document.getElementById("myModal"), {
+  keyboard: false,
+});
+modal.show();
+}
+
+// Função para renderizar os livros filtrados
+function renderBooks(filteredBooks) {
+var container = document.getElementById("cards-container");
+container.innerHTML = "";
+
+for (var i = 0; i < filteredBooks.length; i++) {
+  var livro = filteredBooks[i];
+
+  var card = document.createElement("div");
+  card.className = "col-lg-4 col-md-6 col-sm-12";
+
+  var cardContent = document.createElement("div");
+  cardContent.className = "card h-100";
+
+  var cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+
+  var cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.textContent = livro.tema;
+
+  var cardText = document.createElement("p");
+  cardText.className = "card-text";
+  cardText.textContent = livro.tipo;
+
+  var imageRatio = document.createElement("div");
+  imageRatio.className = "ratio ratio-1x1";
+
+  var cardImage = document.createElement("img");
+  cardImage.className = "card-img-top";
+  cardImage.src = livro.imagem;
+  cardImage.style.objectFit = "contain";
+
+  var contentDiv = document.createElement("div");
+  contentDiv.className = "content";
+
+  var cardNome = document.createElement("p");
+  cardNome.className = "card-nome";
+  cardNome.textContent = livro.nome;
+
+  var button = document.createElement("button");
+  button.className = "btn btn-outline-primary w-100";
+  button.textContent = "Veja aqui";
+  button.addEventListener("click", (function (livro) {
+    return function () {
+      abrirModal(livro.detalhes, livro.link);
+    };
+  })(livro));
+
+  contentDiv.appendChild(cardNome);
+  contentDiv.appendChild(button);
+
+  imageRatio.appendChild(cardImage);
+
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  cardBody.appendChild(imageRatio);
+  cardBody.appendChild(contentDiv);
+
+  cardContent.appendChild(cardBody);
+
+  card.appendChild(cardContent);
+
+  container.appendChild(card);
+}
+}
+
+// Função para filtrar os livros por tema e tipo
+function filterBooks() {
+const selectedTheme = themeFilter.value;
+const selectedType = typeFilter.value;
+
+const filteredBooks = biblioteca.livros.filter((book) => {
+  if (selectedTheme === "all" && selectedType === "all") {
+    return true;
+  } else if (selectedTheme === "all") {
+    return book.tipo.toLowerCase().includes(selectedType.toLowerCase());
+  } else if (selectedType === "all") {
+    return book.tema.toLowerCase().includes(selectedTheme.toLowerCase());
+  } else {
+    return (
+      book.tema.toLowerCase().includes(selectedTheme.toLowerCase()) &&
+      book.tipo.toLowerCase().includes(selectedType.toLowerCase())
+    );
+  }
+});
+
+renderBooks(filteredBooks);
+}
+
+// Função para pesquisar livros por nome
+function searchBooks() {
+const searchTerm = searchInput.value.toLowerCase();
+const filteredBooks = biblioteca.livros.filter((book) =>
+  book.nome.toLowerCase().includes(searchTerm)
+);
+renderBooks(filteredBooks);
+}
+
+// Obtém a referência para o botão de pesquisa
+const searchButton = document.getElementById("search-button");
+
+// Adiciona um ouvinte de evento de clique ao botão
+searchButton.addEventListener("click", searchBooks);
+
+// Obtém a referência para os filtros de tema e tipo
+const themeFilter = document.getElementById("theme-filter");
+const typeFilter = document.getElementById("type-filter");
+
+// Adiciona ouvintes de eventos de mudança aos filtros
+themeFilter.addEventListener("change", filterBooks);
+typeFilter.addEventListener("change", filterBooks);
+
+// Obtém a referência para o campo de pesquisa
+const searchInput = document.getElementById("search-input");
+
+// Adiciona um ouvinte de evento de entrada ao campo de pesquisa
+searchInput.addEventListener("input", searchBooks);
+
+// Chama a função para criar os cards
+criarCards();
